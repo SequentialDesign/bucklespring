c0728c9fcfa11a2b9f447364b924608145d4dcc5
Daniel Alejandro Tapia <sequential.design.dat@gmail.com>
2025-02-03 01:31:37 -0500

    oops deleted excess code

--- scan-x11.c
+++ scan-x11.c
@@ -80,87 +80,9 @@ void key_pressed_cb(XPointer arg, XRecordInterceptData *d)
             break;
         case ButtonPress:
             play(key + 7 + initial_button_hex, 1);
-            /*switch(key) {
-            case -7: // left-click button 1
-                play(0xf0, 1);
-                break;
-            case -6: // middle-click button 2
-                play(0xf1, 1);
-                break;
-            case -5: // right-click button 3
-                play(0xf2, 1);
-                break;
-            case -4: // scroll up button 4
-                play(0xf3, 1);
-                break;
-            case -3: // scroll down button 5
-                play(0xf4, 1);
-                break;
-            case -2: // button 6 horizontal scroll left
-                play(0xf5, 1);
-                break;
-            case -1: // button 7 horizontal scroll right
-                play(0xf6, 1);
-                break;
-            case 0: // button 8 back
-                play(0xf7, 1);
-                break;
-            case 1: // button 9 forward
-                play(0xf8, 1);
-                break;
-            case 2: // button 10 high
-                play(0xf9, 1);
-                break;
-            case 3: // button 11 low
-                play(0xfa, 1);
-                break;
-            case 4: // button 12 ring
-                play(0xfb, 1);
-                break;
-            default:
-                break;
-            }*/
             break;
         case ButtonRelease:
-            play(key + 7 + initial_button_hex, 1);
-            /*switch(key) {
-            case -7: // left-click button 1
-                play(0xf0, 0);
-                break;
-            case -6: // middle-click button 2
-                play(0xf1, 0);
-                break;
-            case -5: // right-click button 3
-                play(0xf2, 0);
-                break;
-            case -4: // scroll up button 4
-                break;
-            case -3: // scroll down button 5
-                break;
-            case -2: // button 6 horizontal scroll left
-                play(0xf5, 0);
-                break;
-            case -1: // button 7 horizontal scroll right
-                play(0xf6, 0);
-                break;
-            case 0: // button 8 back
-                play(0xf7, 0);
-                break;
-            case 1: // button 9 forward
-                play(0xf8, 0);
-                break;
-            case 2: // button 10 high
-                play(0xf9, 0);
-                break;
-            case 3: // button 11 low
-                play(0xfa, 0);
-                break;
-            case 4: // button 12 ring
-                play(0xfb, 0);
-                break;
-            default:
-                break;
-            }*/
+            play(key + 7 + initial_button_hex, 0);
             break;
         default:
             break;
